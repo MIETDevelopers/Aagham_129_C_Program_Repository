35aa59574673c84bba97212b3ffa23efead0969b
unknown <2020a1r129@mietjammu.in>
2021-03-18 18:30:26 +0530

    helloteration file added

--- 3_helloteration.c
+++ 3_helloteration.c
@@ -1,10 +1,11 @@
+//Hello world program created using do-while loop to print hello world multiple times.
 #include <stdio.h>
 int main()
 {
     int a = 1;
-    while ( a <= 10 )
+    while ( a <= 10 )// this line will allow us to print hello world 10times.
     {
-        printf ( "Hello World\n" );
+        printf ( "Hello World\n" );// here we entered the texted to be printed.
         a ++;
     }
     return 0;
