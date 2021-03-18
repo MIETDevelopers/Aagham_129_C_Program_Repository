1c375b7e0b7f1f06d0ab6025c290da5587711a24
unknown <2020a1r129@mietjammu.in>
2021-03-18 17:57:37 +0530

    hello

--- 1_hello.c
+++ 1_hello.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
+//A basic C Program to print Hello World.			
+//Start
+#include<stdio.h>// pre-processor directive to include input output function,
 int main(){
-	printf("hello world");
+	printf("hello world");// printing hello world,
 	return 0;
-}
+}//end,
