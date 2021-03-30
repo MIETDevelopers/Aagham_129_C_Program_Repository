9bf166393cb0b0444edc2fe1751c4243cb2cabc4
unknown <2020a1r129@mietjammu.in>
2021-03-30 16:26:29 +0530

    intialization and printing the values of integer

---
+++ helloWorld.c
@@ -0,0 +1,7 @@
+//A basic C Program to print Hello World.			
+//Start
+#include<stdio.h>// pre-processor directive to include input output function,
+int main(){
+	printf("hello world");// printing hello world,
+	return 0;
+}//end,
